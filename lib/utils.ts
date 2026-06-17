363c33d9bec6912e17c217b3fea12d61a22c5125
sutchan <sutchan@users.noreply.github.com>
2026-06-17 01:11:45 +0000

    feat: 移动目录到 /app
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

---
+++ lib/utils.ts
@@ -0,0 +1,7 @@
+// lib/utils.ts v3.4.0
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
